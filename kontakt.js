42c35481edfef0e4ca1f3f703cc9fcf552f95584
Jakub Wasilewski <jakwasi01@gmail.com>
2023-12-14 16:05:28 +0100

    e-mail form send

---
+++ kontakt.js
@@ -0,0 +1,28 @@
+function SendMail() {
+  // Pobierz wartości z pól formularza
+  let emailValue = document.getElementById("email_id").value;
+  let messageValue = document.getElementById("message").value;
+
+  if (!emailValue || !messageValue) {
+    alert("Wprowadź adres e-mail i wiadomość przed wysłaniem.");
+    return;
+  }
+
+  let params = {
+    email_id: emailValue,
+    message: messageValue,
+  };
+
+  emailjs
+    .send("service_ul8bste", "template_3w483ke", params)
+    .then(function (res) {
+      alert("Success! " + res.status);
+    });
+}
+
+let sendButton = document.getElementById("sendButton");
+
+sendButton.addEventListener("click", function () {
+  console.log("klik!");
+  SendMail();
+});
